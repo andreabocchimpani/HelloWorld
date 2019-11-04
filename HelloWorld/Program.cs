5e6c52218feda91159f26dab0ea0ad35f5d038f1
andreabocchimpani <57347307+andreabocchimpani@users.noreply.github.com>
2019-11-04 12:35:14 +0100

    Oggetto Persona

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Model;
+using System;
 
 namespace HelloWorld
 {
@@ -6,7 +7,32 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            //Value Type
+            int numeroIntero;
+            float numeroConLaVirgola32;
+            double numeroConLaVirgola64;
+            bool valoreLogico;
+            char carattere;
+            decimal valroeDecimale; //Valore preciso a 128 bit utilizzato nei calcoli monetari
+
+            //Reference type
+            string insiemeDiCaratteri;
+            object oggetto;
+
+
+            Person luca = new Person("Luca", "Lippi");
+            luca.eta = 18;
+
+            Console.WriteLine(luca.Anagrafica());
+
+            Person giovanni = luca;
+            giovanni.nome = "Giovanni";
+
+            Console.WriteLine(luca.Anagrafica());
+            Console.WriteLine(giovanni.Anagrafica());
+
         }
     }
 }
